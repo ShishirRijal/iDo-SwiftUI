10888b695ebb514397e5e1dfbe17c4942e622ad0
Shishir Rijal <ccrrizal0892@gmail.com>
2024-09-09 08:24:07 +0545

    Initial Commit

---
+++ iDo/iDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDoApp.swift
+//  iDo
+//
+//  Created by Shishir Rijal on 09/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct iDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
