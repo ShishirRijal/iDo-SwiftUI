83aa0bcccb2f87c9d7d7e0bb0148701cd8cbdf35
Shishir Rijal <ccrrizal0892@gmail.com>
2024-09-11 17:49:02 +0545

    persist data with userdefaults

--- iDo/ViewModels/ListViewModel.swift
+++ iDo/ViewModels/ListViewModel.swift
@@ -9,21 +9,30 @@ import Foundation
 
 class ListViewModel: ObservableObject {
     
-    @Published var items: [ItemModel] = []
+    @Published var items: [ItemModel] = [] {
+        // it gets called everytime items is SET
+        didSet {
+            saveItems()
+        }
+    }
+    let itemLists: String = "ITEM_LISTS"
     
     init() {
         getItems()
     }
     
     func getItems() {
-        let newItems = [
-            ItemModel(title: "Watch a iOS video", isCompleted: true),
-            ItemModel(title: "Let me brush up my skills", isCompleted: false),
-            ItemModel(title: "Start studying for exams", isCompleted: false),
-            ItemModel(title: "Have lunch", isCompleted: true)
-        ]
-        
-        items.append(contentsOf: newItems)
+//        let newItems = [
+//            ItemModel(title: "Watch a iOS video", isCompleted: true),
+//            ItemModel(title: "Let me brush up my skills", isCompleted: false),
+//            ItemModel(title: "Start studying for exams", isCompleted: false),
+//            ItemModel(title: "Have lunch", isCompleted: true)
+//        ]
+//        items.append(contentsOf: newItems)
+        guard let data = UserDefaults.standard.data(forKey: itemLists) else {return}
+        guard let savedItems = try? JSONDecoder().decode([ItemModel].self, from: data) else {return}
+        self.items = savedItems
+
     }
     
     func deleteItem(indexSet: IndexSet) {
@@ -37,11 +46,14 @@ class ListViewModel: ObservableObject {
         items.append(ItemModel(title: title, isCompleted: false))
     }
     func updateItem(item: ItemModel) {
-        
         if let index = items.firstIndex(where: {$0.id == item.id}) {
             items[index] = item.updateCompletion()
         }
-        
+    }
+    func saveItems() {
+        if let encodedData = try? JSONEncoder().encode(items) {
+            UserDefaults.standard.set(encodedData, forKey: itemLists)
+        }
     }
     
 }
